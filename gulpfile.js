05da07358610b69ccf4e99ffeb9bc4ebfe989f07
Wtower <gkarak@9-dev.com>
2017-01-30 10:58:35 +0200

    Defined gulp tasks explicitly

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,74 @@ var paths = {
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var argv = require('yargs').argv;
-var taskMethods = require('gulpfile-ninecms');
+var del = require('del');
+var notify = require('gulp-notify');
+var gulpif = require('gulp-if');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var inlineCss = require('gulp-inline-css');
+
+var production = !!argv.production;
+var config = {
+  autoprefixer_versions: ['last 2 version', 'safari 5', 'ie 8', 'ie 9']
+};
+
+var handleError = function (task) {
+  return function (err) {
+    notify.onError({
+      message: task + ' failed, check the logs',
+      sound: false
+    })(err);
+    gutil.log(gutil.colors.bgRed(task + ' error:'), gutil.colors.red(err));
+  };
+};
+
+var taskMethods = {
+  /*
+   * Delete build folder
+   */
+  clean: function (paths) {
+    return del([paths.build]);
+  },
+
+  /*
+   * SASS
+   */
+  sass: function (paths) {
+    return gulp.src(paths.sass)
+    // sourcemaps + sass + error handling
+      .pipe(gulpif(!production, sourcemaps.init()))
+      .pipe(sass({
+        sourceComments: !production,
+        outputStyle: production ? 'compressed' : 'nested'
+      }))
+      .on('error', handleError('SASS'))
+      // generate .maps
+      .pipe(gulpif(!production, sourcemaps.write({
+        'includeContent': false,
+        'sourceRoot': '.'
+      })))
+      .pipe(gulpif(!production, sourcemaps.init({'loadMaps': true})))
+      .pipe(gulpif(production, autoprefixer(config.autoprefixer_versions)))
+      // we don't serve the source files so include scss content inside the sourcemaps
+      .pipe(sourcemaps.write({'includeContent': true}))
+      .pipe(gulp.dest(paths.build + 'css/'));
+  },
+
+  /*
+   * inline css into html, suitable for emails
+   */
+  inlineCss: function (paths) {
+    return gulp.src(paths.inlineCss.html)
+      .on('error', handleError('inlineCss'))
+      .pipe(inlineCss({
+        preserveMediaQueries: true,
+        applyTableAttributes: true
+      }))
+      .pipe(gulp.dest(paths.inlineCss.build))
+  }
+};
 
 var tasks = {
   clean: function () { return taskMethods.clean(paths); },
