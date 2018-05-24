ee5e9555470ef600526d8e625b63fdbc0f2a673e
Zhmayev Yaroslav <salaros@yandex.ru>
2018-05-24 16:38:28 +0300

    adding HTML and CSS tidying / prettifying using js-jsbeautifier via Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,35 @@
 var gulp = require('gulp'),
     sass = require("gulp-sass"),
+    prettify = require('gulp-jsbeautifier'),
     autoprefixer = require('gulp-autoprefixer'),
     rename = require('gulp-rename'),
     sourcemaps = require('gulp-sourcemaps'),
     inlinesource = require('gulp-inline-source'),
     inlineCss = require('gulp-inline-css');
 
-gulp.task('default', [ 'inline:css' ]);
+gulp.task('default', [ 'html:prettify' ]);
+
+// Tidy up resulting HTML files
+gulp.task('html:prettify', [ 'html:inline:css' ], function () {
+    return gulp.src('./*.html')
+        .pipe(prettify({
+            indent_char: " ",
+            indent_size: 2,
+            selector_separator_newline: true,
+            end_with_newline: true,
+        }))
+        .pipe(gulp.dest('./'));;
+});
 
 // Inline CSS rules in HTML file
-gulp.task('inline:css', [ 'inline:sources' ], function() {
+gulp.task('html:inline:css', [ 'html:inline:sources' ], function () {
     return gulp.src([
-        './*.html',
-        '!./*-inlined.html'
-    ])
-        .pipe(inlineCss({ preserveMediaQueries: true }))
+            './*.html',
+            '!./*-inlined.html'
+        ])
+        .pipe(inlineCss({
+            preserveMediaQueries: true
+        }))
         .pipe(rename({
             suffix: "-inlined",
         }))
@@ -22,10 +37,11 @@ gulp.task('inline:css', [ 'inline:sources' ], function() {
 });
 
 // Inline external JavaScript and CSS files
-gulp.task('inline:sources', [ 'scss:compile' ], function () {
+gulp.task('html:inline:sources', [ 'scss:compile' ], function () {
     return gulp.src('./src/*.html')
         .pipe(inlinesource({
-            compress: false
+            compress: false,
+            pretty: true
         }))
         .pipe(gulp.dest('./'));
 });
@@ -36,7 +52,7 @@ gulp.task('scss:compile', function () {
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
+            browsers: [ 'last 2 versions' ],
             cascade: false
         }))
         .pipe(gulp.dest("css/"))
